b466558b91b38d10120a16a2513bd2d6e272ced6
Walter Zhang <walter.zhangfang@gmail.com>
2022-10-05 15:22:45 +1300

    Rename the "name" of package.json

---
+++ components/Banner.js
@@ -0,0 +1,14 @@
+export default function Banner() {
+  return (
+    <header className="py-5 bg-light border-bottom mb-4">
+      <div className="container">
+        <div className="text-center my-5">
+          <h1 className="fw-bolder">Welcome to Fang Zhang blog </h1>
+          <p className="lead mb-0">
+            Sharing some contents around MERN stack and myself.{" "}
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+}
