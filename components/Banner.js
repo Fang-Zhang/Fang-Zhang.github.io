e66f025b468aa40f8e5b9cd1a6830f50788d7525
WalterZhang <walter.zhangfang@gmail.com>
2022-10-06 13:45:01 +1300

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -3,9 +3,10 @@ export default function Banner() {
     <header className="py-5 bg-light border-bottom mb-4">
       <div className="container">
         <div className="text-center my-5">
-          <h1 className="fw-bolder">Welcome to MERN project </h1>
+          <h1 className="fw-bolder">Welcome the entrepreneurs and MERN developers </h1>
           <p className="lead mb-0">
-            Sharing the contents around MERN stack(MongoDB, Express.js, React.js and Node.js).
+            Boost the entrepreneur through MERN stack(MongoDB, Express.js,
+            React.js and Node.js).
           </p>
         </div>
       </div>
