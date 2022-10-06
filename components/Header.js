2583f658d28a489a3ef38bef3051c94d6cfcd50c
WalterZhang <walter.zhangfang@gmail.com>
2022-10-06 14:48:37 +1300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,23 +9,11 @@ export default function Header() {
           <Link href="/">
             <a className="navbar-brand">MERN Projects</a>
           </Link>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <Link href="/">
                 <a className="nav-link">
-                  <li className="nav-item"> Home </li>
+                  <li className="nav-item">Home </li>
                 </a>
               </Link>
               <Link href="/collections/">
