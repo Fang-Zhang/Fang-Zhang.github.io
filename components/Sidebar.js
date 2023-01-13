88a23915ce450315e3ac1abe7d4fa3fe7e038781
Walter Zhang <walter.zhangfang@gmail.com>
2023-01-13 22:58:29 +1300

    Update the sidebar.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -12,7 +12,9 @@ export default function Sidebar() {
   return (
     <div className="col-lg-4">
       <div className="card mb-4">
-        <div className="card-header">Search</div>
+        <div className="card-header">
+          <h4>Search</h4>
+        </div>
         <div className="card-body">
           <div className="input-group">
             <input
@@ -38,7 +40,9 @@ export default function Sidebar() {
       </div>
 
       <div className="card mb-4">
-        <div className="card-header">Categories</div>
+        <div className="card-header">
+          <h4>Categories</h4>
+        </div>
         <div className="card-body">
           <div className="row">
             <div className="col-sm-10">
@@ -57,14 +61,105 @@ export default function Sidebar() {
                     );
                   });
                 })} */}
+                <li>Soft Skills
+                  <ul>
+                    <Link href="/category/code-fundamentals">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Be a proper coder</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Glance the outside</li>
+                      </a>
+                    </Link>
+                  </ul>
+                </li>
+                <li>Information Technology
+                  <ul>
+                    <Link href="/category/code-fundamentals">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Code Fundamentals</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>FullStack Dev</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>CI/CD</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Design Patterns</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Security</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Architecture</li>
+                      </a>
+                    </Link>
+                  </ul>
+                </li>
+                <li>Book Review
+                  <ul>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Fiction</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Non-Fiction</li>
+                      </a>
+                    </Link>
+                  </ul>
+                </li>
+                <li>Entrepreneur
+                  <ul>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Self Publish</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>API</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>SaaS</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>NFT</li>
+                      </a>
+                    </Link>
+                    <Link href="/category/basic-skill">
+                      <a target="_blank" style={{ color: "black" }}>
+                        <li>Blockchain</li>
+                      </a>
+                    </Link>
+                  </ul>
+                </li>
                 <Link href="/category/basic-skill">
                   <a target="_blank" style={{ color: "black" }}>
-                    <li>Basic Skills</li>
+                    <li>Average Life</li>
                   </a>
                 </Link>
-                <Link href="/category/js-dev">
+                <Link href="/category/basic-skill">
                   <a target="_blank" style={{ color: "black" }}>
-                    <li>JS Dev</li>
+                    <li>Classical Chinese Culture</li>
                   </a>
                 </Link>
               </ul>
@@ -74,11 +169,26 @@ export default function Sidebar() {
       </div>
 
       <div className="card mb-4">
-        <div className="card-header">Useful Resources</div>
+        <div className="card-header">
+          <h4>Useful Links</h4>
+        </div>
         <div className="card-body">
           <ul>
-            <li><a href="http://mdearth.io" target="_blank" rel="noreferrer" >MDearth</a></li>
-            <li><a href="https://www.zhouyi.cc/zhouyi/yijing64/" target="_blank" rel="noreferrer" >iChing</a></li>
+            <Link href="https://mdearth.io">
+              <a target="_blank" style={{ color: "black" }}>
+                <li>MDearth</li>
+              </a>
+            </Link>
+            <Link href="https://www.zhouyi.cc/zhouyi/yijing64/">
+              <a target="_blank" style={{ color: "black" }}>
+                <li>易經64卦</li>
+              </a>
+            </Link>
+            <Link href="/category/basic-skill">
+              <a target="_blank" style={{ color: "black" }}>
+                <li>Collections for Coding</li>
+              </a>
+            </Link>
           </ul>
         </div>
       </div>
