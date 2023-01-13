93a6df013f90b85e33fd16eab8d0daa8e8492360
Walter Zhang <walter.zhangfang@gmail.com>
2023-01-13 17:38:02 +1300

    Update index.js

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -13,7 +13,37 @@ export default function Portfolios({ posts }) {
         <br />
         When I came to New Zealand since 2019, I started to understand and
         involve MERN(MongoDB, ExpressJS, ReactJS and NodeJS) Full-Stack
-        development.
+        development around Web3.0.
+      </div>
+      <br />
+      <div style={{ textAlign: "start", margin: "2rem", fontSize: "1.5rem" }}>
+        E-mail: <a style={{ color: "blue"}}>walter.zhangfang@gmail.com</a>
+        <br />
+        People also could get touch with me through:
+        - 
+        <Link href="https://twitter.com/WalterFangZhang">
+          <a target="_blank" style={{ color: "blue" }}>
+            Twitter
+          </a>
+        </Link>
+         - or - 
+        <Link href="https://github.com/Fang-Zhang">
+          <a target="_blank" style={{ color: "blue" }}>
+            Github
+          </a>
+        </Link>
+         - or - 
+        <Link href="https://www.linkedin.com/in/fang-zhang-ba76aaa4/">
+          <a target="_blank" style={{ color: "blue" }}>
+            Linkedin
+          </a>
+        </Link>
+        - of course, and - 
+        <Link href="/">
+          <a target="_blank" style={{ color: "blue" }}>
+            This Blog
+          </a>
+        </Link>
       </div>
       <br />
       <div style={{ textAlign: "start", margin: "2rem", fontSize: "1.5rem" }}>
@@ -28,7 +58,14 @@ export default function Portfolios({ posts }) {
       </div>
       <br />
       <div style={{ textAlign: "start", margin: "2rem", fontSize: "1.5rem" }}>
-        <h3> MERN Full-Stack Dev: </h3>
+        <h3> Full-Stack Dev: </h3>
+        <Link href="https://mdearth.io/">
+          <a target="_blank" style={{ color: "blue" }}>
+            MDearth.io
+          </a>
+        </Link>
+        : Help the local artists to touch with the web3 world.
+        <br />
         <Link href="rainbow-profiterole-c64be5.netlify.app">
           <a target="_blank" style={{ color: "blue" }}>
             Memories
